f478b1a6da232d215467056a06cfa2389965902d
Juandlc <juniorelpapa09@hotmail.com>
2020-08-16 18:36:53 -0500

    Updates

---
+++ precache-manifest.6ed5b6439ec13d5f18d565d2a111f200.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "577c56e4860dd124a5825e7efc4cb7eb",
+    "url": "/spotify-clone/index.html"
+  },
+  {
+    "revision": "20d22ef654635b71f1c1",
+    "url": "/spotify-clone/static/css/main.ea2c0e26.chunk.css"
+  },
+  {
+    "revision": "815b252f16e6f971f9ab",
+    "url": "/spotify-clone/static/js/2.168a43f0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/spotify-clone/static/js/2.168a43f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20d22ef654635b71f1c1",
+    "url": "/spotify-clone/static/js/main.75b2de2e.chunk.js"
+  },
+  {
+    "revision": "52de21c6b549d6d1baa2",
+    "url": "/spotify-clone/static/js/runtime-main.f1339594.js"
+  }
+]);
